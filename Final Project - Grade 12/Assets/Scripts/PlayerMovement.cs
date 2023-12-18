acfbed0bb691ffcbbc37ab20c8c0ba5fb2a450f6
ifItWorksWhyQuestionIt <120057892+ifItWorksWhyQuestionIt@users.noreply.github.com>
2023-12-18 11:48:12 -0500

    Added Player Movement and Basic Animations

--- Final Project - Grade 12/Assets/Scripts/PlayerMovement.cs
+++ Final Project - Grade 12/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,53 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+    [SerializeField] private float movementSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float maxFallSpeed = 30;
 
     private void Start()
-    {
+    { 
         // Set RigidBody Variable
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         // Player Movement
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * movementSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector3(rb.velocity.x, 14f);
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+
+        if (rb.velocity.y < -maxFallSpeed) rb.velocity = new Vector2(rb.velocity.x, -maxFallSpeed);
+
+        AnimationStateUpdater();
+    }
+
+    private void AnimationStateUpdater()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("Moving", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("Moving", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
         }
     }
 }
