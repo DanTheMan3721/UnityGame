8a8e2bb8d83454cd8e59ee3d3cf8cf8097a9277c
ifItWorksWhyQuestionIt <120057892+ifItWorksWhyQuestionIt@users.noreply.github.com>
2023-12-18 10:50:34 -0500

    Added Player Movement

---
+++ Final Project - Grade 12/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        // Set RigidBody Variable
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Player Movement
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 14f);
+        }
+    }
+}
