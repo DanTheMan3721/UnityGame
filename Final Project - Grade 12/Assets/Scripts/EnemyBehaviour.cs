f49d9c8ef9b25cc98d0c8a7b4a2647bed5dbed25
DanTheMan3721 <dli20060805@gmail.com>
2024-01-09 12:26:56 -0500

    More Skeleton Movement

--- Final Project - Grade 12/Assets/Scripts/EnemyBehaviour.cs
+++ Final Project - Grade 12/Assets/Scripts/EnemyBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,51 +9,102 @@ public class EnemyBehaviour : MonoBehaviour
     public GameObject player;
     public float speed;
     public float checkRadius;
-    public float attackRadius;
+
+    Rigidbody2D rb;
 
     private float distance;
+    private float dirX = 0f;
 
+    [SerializeField] private Transform wallCheckRight;
+    [SerializeField] private Transform wallCheckLeft;
+    [SerializeField] private LayerMask wallLayer;
+
+    private bool IsWalled()
+    {
+        return (Physics2D.OverlapCircle(wallCheckRight.position, 0.2f, wallLayer) || Physics2D.OverlapCircle(wallCheckLeft.position, 0.2f, wallLayer));
+    }
     private SpriteRenderer sprite;
     private Animator anim;
-    private Transform target;
-    private Rigidbody2D rb;
-    private Vector2 movement;
-    LayerMask whatIsPlayer;
 
-    private bool isInChaseRange;
-    private bool isInAttackRange;
-    private enum MovementState { idle, running}
-    // Start is called before the first frame update
-    void Start()
+    public enum WalkableDirection {Right, Left }
+
+    private WalkableDirection _walkDirection;
+    private Vector2 walkDirectionVector = Vector2.right;
+
+    public WalkableDirection walkDirection
+    {
+        get { return _walkDirection; }
+        set {
+            if (_walkDirection != value)
+            {
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+
+                if (value == WalkableDirection.Right)
+                {
+                    walkDirectionVector = Vector2.right;
+                } else if(value == WalkableDirection.Left)
+                {
+                    walkDirectionVector = Vector2.left;
+                }
+            }
+            _walkDirection = value; }
+    }
+
+    private enum MovementState {idle, running, attack, Death}
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
+
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        //enemy moves towards player if in circle
-        /*isInChaseRange = Physics2D.OverlapBox(transform.position, checkRadius, whatIsPlayer);*/
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if (distance < checkRadius)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            if (IsWalled())
+            {
+                FlipDirection();
+            }
+            rb.velocity = new Vector2(speed * walkDirectionVector.x, rb.velocity.y);
+        }
+        
+    }
 
-        //animation stuff
-        AnimationStateUpdater();
+    private void FlipDirection()
+    {
+        if (walkDirection == WalkableDirection.Right)
+        {
+            walkDirection = WalkableDirection.Left;
+        }
+        else if (walkDirection == WalkableDirection.Left)
+        {
+            walkDirection = WalkableDirection.Right;
+        }
+        else
+        {
+            Debug.LogError("Walkable direction not set to legal value");
+        }
     }
 
     private void AnimationStateUpdater()
     {
-        /*MovementState state;
+        MovementState state;
 
-        if (dirX > 0.1f)
+        if (walkDirection == WalkableDirection.Right)
         {
             state = MovementState.running;
             sprite.flipX = false;
         }
-        else if (dirX < 0.1f)
+        else if (walkDirection == WalkableDirection.Left)
         {
             state = MovementState.running;
             sprite.flipX = true;
@@ -62,6 +114,6 @@ public class EnemyBehaviour : MonoBehaviour
             state = MovementState.idle;
         }
 
-        anim.SetInteger("State", (int)state);*/
+        anim.SetInteger("State", (int)state);
     }
 }
