12d57c10f5d2e04479f4747a5f4b8bbfa67112f4
DanTheMan3721 <dli20060805@gmail.com>
2024-01-15 12:15:19 -0500

    made main menu and options menu

---
+++ Final Project - Grade 12/Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Forest");
+    }
+    public void levelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+    public void options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+        public void quitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
